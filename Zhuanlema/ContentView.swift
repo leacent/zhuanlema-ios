d8b4844d03e1ef79096cfc7afbe196ec80305772
leacent <leason316@foxmail.com>
2026-01-24 22:20:13 +0800

    Initial Commit

---
+++ Zhuanlema/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Zhuanlema
+//
+//  Created by leacent song on 2026/1/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Zhuanlema/ZhuanlemaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZhuanlemaApp.swift
+//  Zhuanlema
+//
+//  Created by leacent song on 2026/1/24.
+//
+
+import SwiftUI
+
+@main
+struct ZhuanlemaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
